a252d30c875cec3fd597817b406896c068b201d7
sergei.budnikov <sergei.budnikov@smartseeds.ru>
2024-03-08 08:12:18 +0300

    Add project files.

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine(GC.GetTotalMemory(false));
+
+for (int i = 0; i < 10000000; i++)
+{
+    //боксим
+    var obj = (object)i;
+    int j = (int)obj;
+}
+
+Console.WriteLine(GC.GetTotalMemory(false));
+
+//когда выходим из зоны видимости юзинга отрабатывает Dispose
+using (var c = new MyClass())
+{
+
+}
+
+Console.WriteLine(GC.GetTotalMemory(false));
+
+GC.Collect();
+
+Console.WriteLine(GC.GetTotalMemory(false));
+
+Console.ReadLine();
+
+class MyClass : IDisposable
+{
+    public MyClass() { }
+
+    ~MyClass() { }
+
+    //финализатор
+    public void Dispose()
+    {
+        GC.Collect();
+    }
+
+
+}
